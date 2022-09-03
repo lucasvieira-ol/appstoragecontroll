80cd96159eddc98048f62decf5eb4d8b72539630
lucasvieira.ol <lucaslvo53@gmail.com>
2022-09-03 00:08:58 -0300

    primeira configuração

---
+++ src/main/java/com/appstoragecontroll/storagecontroll/StoragecontrollApplication.java
@@ -0,0 +1,13 @@
+package com.appstoragecontroll.storagecontroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoragecontrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoragecontrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appstoragecontroll/storagecontroll/StoragecontrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appstoragecontroll.storagecontroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoragecontrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
